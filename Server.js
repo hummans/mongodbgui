25d360b5d58e5ba32f2dc7e4a5d792487a955eda
gayankuruppu <example@example.com>
2019-12-12 21:22:06 +0530

    Update Server.js

--- Server.js
+++ Server.js
@@ -3,6 +3,11 @@ var MongoClient = require('mongodb').MongoClient;
 
 const app = express();
 
+
+app.get('/check/', (req, res) => {
+    res.send("Check database");
+});
+
 app.get('/insert/', (req, res) => {
     var url = "mongodb+srv://appuser:1234@cluster0-vdt7y.mongodb.net/test?retryWrites=true&w=majority";
     MongoClient.connect(url, function(err, db) {
@@ -11,18 +16,9 @@ app.get('/insert/', (req, res) => {
         var myobj = { name: "Company Inc", address: "Highway 37" };
         dbo.collection("native").insertOne(myobj, function(err, res) {
             if (err) throw err;
-            res.send("1 document inserted");
+            console.log("1 document inserted");
             db.close();
         });
-    });
-});
-
-app.get('/database/', (req, res) => {
-    var url = "mongodb://localhost:27017/mydb";
-    MongoClient.connect(url, function(err, db) {
-        if (err) throw err;
-        res.send("Database created!");
-        db.close();
     });
 });
 
