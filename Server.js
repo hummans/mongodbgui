1a1f59636b8bfd49106fa99984a631ede962f9b5
gayankuruppu <example@example.com>
2019-12-12 22:32:34 +0530

    Update Server.js

--- Server.js
+++ Server.js
@@ -8,17 +8,29 @@ app.get('/check/', (req, res) => {
     res.send("Check database");
 });
 
-app.get('/insert/:database', (req, res) => {
+app.get('/insert/:username/:password/:cluster/:database/:collection', (req, res) => {
+    var username = req.params.username;
+    var password = req.params.password;
+    var cluster = req.params.cluster;
     var database = req.params.database;
-    var url = "mongodb+srv://appuser:1234@cluster0-vdt7y.mongodb.net/test?retryWrites=true&w=majority";
+    var collection = req.params.collection;
+
+    // cluster0-vdt7y
+    var temp_url = "mongodb+srv://username:password@cluster.mongodb.net/test?retryWrites=true&w=majority";
+    var temp_username = temp_url.replace("username", username);
+    var temp_password = temp_username.replace("password", password);
+    var url = temp_password.replace("cluster", cluster);
+
+    console.log(url);
+
     MongoClient.connect(url, function(err, db) {
         if (err) throw err;
         var dbo = db.db(database);
         var myobj = { name: "Company Inc", address: "Some Random data" };
-        return dbo.collection("native").insertOne(myobj, function(err) {
-            if (err)  return res.send("Error " + database + err);
+        return dbo.collection(collection).insertOne(myobj, function(err) {
+            if (err)  return res.send("Error " + database + collection + err);
             db.close();
-            return res.send("1 document inserted to " + database);
+            return res.send("1 document inserted to " + database + collection);
         });
     });
 });
