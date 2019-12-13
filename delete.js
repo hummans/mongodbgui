ab07dab4ddfa3e8a7b4a525b7e5d99e12ae0f8d6
gayankuruppu <example@example.com>
2019-12-13 12:10:52 +0530

    Add delete.js

---
+++ delete.js
@@ -0,0 +1,77 @@
+var MongoClient = require('mongodb').MongoClient;
+module.exports = {
+    /*{
+         "username" : "appuser",
+         "password" : "1234",
+         "cluster" : "cluster0-vdt7y",
+         "database":"react",
+         "collection":"native",
+         "data":{"Price": "101"}
+      }
+     */
+    deleteMany: function (req, res) {
+        var auth = Object.entries(req.body);
+
+        var url = "mongodb+srv://username:password@cluster.mongodb.net/test?retryWrites=true&w=majority";
+        var replace = {
+            username:auth[0][1],
+            password:auth[1][1],
+            cluster:auth[2][1]
+        };
+
+        url =  url.replace(/username|password|cluster/gi, function(matched){
+            return replace[matched];
+        });
+
+        try{
+            MongoClient.connect(url, {useUnifiedTopology: true}, function(err, db) {
+                if (err) return  res.send('{"status":"error", "desc":'+ err + '}');
+                var dbo = db.db(auth[3][1]);
+                return dbo.collection(auth[4][1]).deleteMany(auth[5][1], function(err, obj) {
+                    if (err)  return  res.send('{"status":"error", "desc":'+ err + '}');
+                    db.close();
+                    return res.send('{"status":"ok", "desc": "' + obj.result.n + ' documents(s) deleted"}');
+                });
+            });
+        } catch (e) {
+            return  res.send('{"status":"error", "desc":'+ e + '}');
+        }
+    },
+    /*{
+         "username" : "appuser",
+         "password" : "1234",
+         "cluster" : "cluster0-vdt7y",
+         "database":"react",
+         "collection":"native",
+         "data":{"Price": "101"}
+      }
+     */
+    deleteOne: function (req, res) {
+        var auth = Object.entries(req.body);
+
+        var url = "mongodb+srv://username:password@cluster.mongodb.net/test?retryWrites=true&w=majority";
+        var replace = {
+            username:auth[0][1],
+            password:auth[1][1],
+            cluster:auth[2][1]
+        };
+
+        url =  url.replace(/username|password|cluster/gi, function(matched){
+            return replace[matched];
+        });
+
+        try{
+            MongoClient.connect(url, {useUnifiedTopology: true}, function(err, db) {
+                if (err) return  res.send('{"status":"error", "desc":'+ err + '}');
+                var dbo = db.db(auth[3][1]);
+                return dbo.collection(auth[4][1]).deleteOne(auth[5][1], function(err, obj) {
+                    if (err)  return  res.send('{"status":"error", "desc":'+ err + '}');
+                    db.close();
+                    return res.send('{"status":"ok", "desc": "' + obj.result.n + ' documents deleted"}');
+                });
+            });
+        } catch (e) {
+            return  res.send('{"status":"error", "desc":'+ e + '}');
+        }
+    }
+};
